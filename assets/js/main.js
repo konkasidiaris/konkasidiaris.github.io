9a8b9e07bed1beac3303fc1dc90fd442cae59e17
Konstantinos Kasidiaris <kkasidiaris@gmail.com>
2025-11-02 16:56:43 +0200

    feat: adds head, adds header, fixes darkmode

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+function toggleDarkMode() {
+    const html = document.documentElement;
+    const isDark = html.style.colorScheme === 'dark';
+    const button = document.getElementById("dark-mode-toggle");
+
+    if (isDark) {
+        html.style.colorScheme = 'light';
+        localStorage.setItem('theme', 'light');
+        button.classList.remove("dark");
+        button.setAttribute("aria-label", "Enable dark mode");
+    } else {
+        html.style.colorScheme = 'dark';
+        button.classList.add("dark");
+        localStorage.setItem('theme', 'dark');
+        button.setAttribute("aria-label", "Enable light mode");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", (_e) => {
+    (function () {
+        const savedTheme = localStorage.getItem('theme');
+        const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        const button = document.getElementById("dark-mode-toggle");
+
+        const theme = savedTheme || (systemPrefersDark ? 'dark' : 'light');
+
+        if (theme === 'dark') {
+            document.documentElement.style.colorScheme = 'dark';
+            button.classList.add("dark");
+            button.setAttribute("aria-label", "Enable light mode");
+        } else {
+            document.documentElement.style.colorScheme = 'light';
+            button.setAttribute("aria-label", "Enable dark mode");
+        }
+    })();
+
+    document.getElementById("hamburger").addEventListener("click", (e) => {
+        e.currentTarget.classList.toggle("open");
+    });
+
+    document.getElementById("dark-mode-toggle").addEventListener("click", toggleDarkMode);
+});
